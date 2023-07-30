d6b3dba8b16a2e4a628ef80fb03e3abdb2f1cd76
kazurayam <kazuaki.urayama@gmail.com>
2023-07-30 11:30:53 +0900

    first commit

---
+++ Scripts/Main Test Cases/TC1_Verify Successful Login/Script1482850539026.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+import internal.GlobalVariable
+
+System.setProperty("webdriver.chrome.logfile", "./chromedriver.log");
+System.setProperty("webdriver.chrome.verboseLogging", "true");
+
+WebUI.comment('Story: Login to CURA system')
+
+WebUI.comment('Given that the user has the valid login information')
+
+WebUI.openBrowser(GlobalVariable.G_SiteURL)
+
+WebUI.click(findTestObject('Page_CuraHomepage/btn_MakeAppointment'))
+
+WebUI.setText(findTestObject('Page_Login/txt_UserName'), Username)
+
+WebUI.setText(findTestObject('Page_Login/txt_Password'), Password)
+
+WebUI.comment('When he logins to CURA system')
+
+WebUI.click(findTestObject('Page_Login/btn_Login'))
+
+WebUI.comment('Then he should be able to login successfully')
+
+landingPage = WebUI.verifyElementPresent(findTestObject('Page_CuraAppointment/div_Appointment'), GlobalVariable.G_Timeout)
+
+WebUI.closeBrowser()
+
